94f6ee9507904c6c369bc96bcd15f1713e1948d9
Taras Viyatyk <tviyatyk@gmail.com>
2020-03-22 20:53:57 +0200

    Add seeds for ProgrammingLanguages

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Let's fill DB with data provided in JSON file
+PROGRAMMING_LANGUAGES_LOCATION = "seeds/programming_languages.json"
+ARRAY_KEYS = [:categories, :designed_by]
+NEW_COLUMN_NAMES = {:type => :categories}
+
+if ProgrammingLanguage.count.zero?
+  path = File.join(File.dirname(__FILE__), PROGRAMMING_LANGUAGES_LOCATION)
+  languages = JSON.parse(File.read(path))
+
+  languages.each do |language|
+    prepared_params = language.to_a.map do |pair|
+      key = pair.first.downcase.parameterize(separator: '_').to_sym
+      key = NEW_COLUMN_NAMES.fetch(key, key)
+
+      value = pair.last
+      value = value.split(',').map(&:strip) if ARRAY_KEYS.include?(key)
+
+      [key, value]
+    end.to_h
+
+    ProgrammingLanguage.create!(prepared_params)
+  end
+
+  puts "Programming languages are seeded"
+end
